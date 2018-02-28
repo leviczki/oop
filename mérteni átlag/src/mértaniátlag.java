5ab25bdeaaae0be3be45ed10fbca09eea93d857b
LuciaJohns <luciajohns@outlook.com>
2018-02-28 13:53:23 +0100

    mértani átlag

---
+++ mérteni átlag/src/mértaniátlag.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author janosi3
+ *
+ */
+public class mértaniátlag {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double tomb [] = new double [3]; 
+		Scanner bemenet = new Scanner(System.in);
+		
+		for (int i = 0; i < tomb.length; i++) {
+			System.out.println("Adja meg a "+ (i + 1) + "elemet");
+			tomb[i] = bemenet.nextDouble();
+		}
+		
+		double mertani_atlag = 1;
+		
+		
+
+	}
+
+}
