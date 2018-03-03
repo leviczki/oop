f943f8f38e32c3d75da6e861900e68c6a13cc17a
lucia <lucia@DESKTOP-O4BB7D5>
2018-03-03 19:53:30 +0100

    pénz felbontása egyes-ötös-tizes-ötvenesre Barry Burd-Java

---
+++ Átváltás/src/átváltás.java
@@ -0,0 +1,21 @@
+import static java.lang.System.out; 
+
+class átváltás{
+
+public static void main(String args[]) {
+	int teljes = 248; 
+	int ötvenes = teljes / 50;
+	int amiMaradt = teljes % 50; 
+	int tízes = amiMaradt / 10; 
+	amiMaradt = amiMaradt % 10;
+	int ötös = amiMaradt / 5; 
+	amiMaradt = amiMaradt % 5; 
+	int egyes = amiMaradt;
+	
+	out.println("Ami a " + teljes + " forintodból lett"); 
+	out.println(ötvenes + " ötvenes");
+	out.println(tízes + " tízes");
+	out.println(ötös + " ötös"); 
+	out.println(egyes + " egyes"); 
+	}
+}
