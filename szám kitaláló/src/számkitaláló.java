7e72d88ac40e06206e64de4e9320530369026c84
lucia <lucia@DESKTOP-O4BB7D5>
2018-03-03 20:09:10 +0100

    Számkitalálós "játék" Barry Burd-Java

---
+++ szám kitaláló/src/számkitaláló.java
@@ -0,0 +1,26 @@
+import static java.lang.System.out; 
+import java.util.Scanner; 
+import java.util.Random;
+
+class számkitaláló {
+	
+public static void main(String args[]) { 
+	Scanner billentyûzet = new Scanner(System.in);
+
+	out.println("írj be egy egészet 1 és 10 között: ");
+	int bekértSzám = billentyûzet.nextInt();
+	int véletlenSzám = new Random().nextInt(10) + 1;
+	if (bekértSzám == véletlenSzám){ 
+		out.println ("**********");
+		out.println("*Nyertél.*");
+		out.println ("**********");
+		}
+	else {
+		out.println("Vesztettél.");
+		out.println("A véletlen szám ez volt: "); 
+		out.println (véletlenSzám);
+		out.println("Köszönöm a játékot.");
+		}  
+	billentyûzet.close();
+	}
+}
