8351248737749eed6c11908da4d2acfef7245e82
lucia <lucia@DESKTOP-O4BB7D5>
2018-03-03 19:43:38 +0100

    Üres keret kirajzolása Barry Burd-Java

---
+++ Keret/src/keret.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+class keret {
+	
+public static void main(String args[]) {
+	JFrame myFrame = new JFrame (); 
+	String myTitle = "Üres Keret";
+	myFrame.setTitle(myTitle);
+	myFrame.setSize(300, 200);
+
+	myFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	myFrame.setVisible(true);
+	}
+}
