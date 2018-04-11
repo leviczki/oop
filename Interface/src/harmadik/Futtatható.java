013c02aba278deec93cf1df326fc0bb1b6c9c143
LuciaJohns <luciajohns@outlook.com>
2018-04-11 13:42:53 +0200

    Interface nincs toll órai

---
+++ Interface/src/harmadik/Futtatható.java
@@ -0,0 +1,31 @@
+package harmadik;
+
+import java.awt.Color;
+
+import masik.SzinesPont;
+
+import sajat.ISzinezheto;
+
+public class Futtatható {
+
+	
+	public static void main(String[] args) {
+		
+		ISzinezheto szinespont = new SzinesPont(Color.BLUE);
+		System.out.println(szinespont);
+		
+		szinespont.setAlapertelmezett(Color.GREEN);
+		System.out.println(szinespont);
+		
+		Atszinezo(szinespont);
+		System.out.println(szinespont);
+	
+		
+		
+	}
+	
+	public static void Atszinezo(ISzinezheto objektum) {
+		objektum.setAlapertelmezett(objektum.alapertelmezett);
+	}
+
+}
---
+++ Interface/src/sajat/ISzinezheto.java
@@ -0,0 +1,11 @@
+package sajat;
+
+import java.awt.Color;
+
+public interface ISzinezheto {
+	Color alapertelmezett = Color.RED;
+	
+	Color getAlapertelmezett();
+	void setAlapertelmezett(Color szin);
+	
+}
