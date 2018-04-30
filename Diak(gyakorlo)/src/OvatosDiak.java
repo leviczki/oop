c8bc063aafcebc6c6b37ebc6fe0e7fda77f539d1
lucia <lucia@DESKTOP-O4BB7D5>
2018-04-30 16:14:48 +0200

    Diakgyakorlo feladat Folyamatban https://www.iit.uni-miskolc.hu/smid-laszlo/oop-gyakorlati-feladatsor.html

---
+++ Diak(gyakorlo)/src/OvatosDiak.java
@@ -0,0 +1,30 @@
+
+//Irjon egy OvatosDiak osztalyt a Diak osztaly leszarmozattjakent
+public class OvatosDiak extends Diak {
+	
+//amely kepes tarolni azt az osszeget, amit a diak a hazautra utikoltsegkent felretesz
+//(mert utal gyalogolni)
+	int utiKoltseg;
+	
+	public OvatosDiak(String nev, int forint) {
+		super(nev, forint);
+		// TODO Auto-generated constructor stub
+	}
+
+//Definialjon konstruktort, amellyel egy peldany adatai beallithatok!
+	
+//Definialja felul a sorozik metodustugy, hogy
+//csak akkor csokkenti az ital araval a penz mennyiseget
+//ha meg marad az utikoltsegre
+//es csak ekkor ad vissza igaz erteket
+//(ha csak reszben fedezne amaradek penz az italt, akkor mar ne vonja le)
+	
+	
+//Definialja az osztalyra a toString metodust ugy,
+//hogy a "Penzmag:" szoveg utan kiirja a utikoltsegen 
+//felul meglevo penz mennyiseget.
+	
+	
+	
+	
+}
---
+++ Diak(gyakorlo)/src/RockWell.java
@@ -0,0 +1,20 @@
+
+//Definialjon egy RockWell osztalyt, mely futtathato
+public class RockWell {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//Hozzon letre egy OvatosDiak peldanyt, tetszoleges adatokkal!
+		type name = new type();
+		
+		
+//Itasson meg a peldannyal 300Ft-os sort annyiszor
+//ahanyszor kitelik a penzebol
+//Írja ki a megivott sorok szamait
+//es az aktualis penz mennyiseget!
+		
+		
+	}
+
+}
