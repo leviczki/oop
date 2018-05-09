4b0fd9756b71339ebfda0805d847b2beaac7a1fa
Balázs <36498726+leviczki@users.noreply.github.com>
2018-05-09 18:16:06 +0200

    Update CreditCard.java
    így talán már nincs benne hiba

--- beszámoló 2018.05.09/src/bank/CreditCard.java
+++ beszámoló 2018.05.09/src/bank/CreditCard.java
@@ -17,20 +17,26 @@ public class CreditCard extends Card {
 	@Override
 	public boolean penzfelvetel(int osszeg) {
 		if(hitelkeret + getEgyenleg() >= osszeg){
-			if(getEgyenleg() >= osszeg){
-				super.penzFelvetel(osszeg);
+			//if(getEgyenleg() >= osszeg){  // "super." lemaradt....
+			if(super.getEgyenleg() >= osszeg){
+				super.setEgyenleg(super.getEgyenleg-osszeg);
 			}else{
-				hitelkeret = hitelkeret - (osszeg - setEgyenleg(0);
+				super.setEgyenleg(super.getEgyenleg-osszeg);
+				hitelkeret += super.getEgyenleg();//minusz a getEgyenleg, tehat levonni fog a hitelkeretbol
+				super.setEgyenleg(0);//ne maradjon minusz, ha levonta a hitelkeretbol
+				//hitelkeret = hitelkeret - (osszeg - setEgyenleg(0);
+				//lemaradt a zárójel és a setEgyenlegneget nem tudod kivonni az osszegbol mert nincs visszateresi erteke (void)
 			}
 			return true;
 		}else return false;
-			}
+			//} //ez itt miiii?
 	}
 
 	@Override
 	public String toString() {
-		return "CreditCard [hitelkeret=" + hitelkeret + ", getEgyenleg()="
-				+ getEgyenleg() + ", getName()=" + getName() + "]";
+		/*return "CreditCard [hitelkeret=" + hitelkeret + ", getEgyenleg()="
+				+ getEgyenleg() + ", getName()=" + getName() + "]";*/
+		return "Tulajdonos: "+super.getName+", egyenleg:"+super.getEgyenleg+", hitelkeret: "+hitelkeret;
 	}
 
 	public int getHitelkeret() {
